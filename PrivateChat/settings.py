7461379c845cbccbe7163d6809672db152ec253d
diegofsousa <diegofernando5672@gmail.com>
2016-12-13 13:57:22 -0300

    preparing for deploy

--- PrivateChat/settings.py
+++ PrivateChat/settings.py
@@ -119,3 +119,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Heroku settings
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
